c8ddcf3a5e78d157fae7b7d46a530cfc9006b9e4
gaku101 <takehiro-r133@hotmail.co.jp>
2025-02-08 13:01:20 +0900

    make wasip1 runnable

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 use clap::Parser;
-use wasmtime::component::{bindgen, Component, Linker};
+use wasmtime::component::{bindgen, Component, Linker, ResourceTable};
 use wasmtime::{Engine, Store};
+use wasmtime_wasi::{WasiCtx, WasiCtxBuilder, WasiView};
 
 use component::greet::greetable::Host;
 
 struct Greet {
     name: String,
+    wasi_ctx: WasiCtx,
+    resource_table: ResourceTable,
 }
 
 impl Greet {
     fn new(name: String) -> Greet {
-        Greet { name }
+        let wasi_ctx = WasiCtxBuilder::new().build();
+        let resource_table = ResourceTable::new();
+        Greet {
+            name,
+            wasi_ctx,
+            resource_table,
+        }
+    }
+}
+
+impl WasiView for Greet {
+    fn table(&mut self) -> &mut ResourceTable {
+        &mut self.resource_table
+    }
+
+    fn ctx(&mut self) -> &mut WasiCtx {
+        &mut self.wasi_ctx
     }
 }
 
@@ -46,11 +65,13 @@ fn start(cli: Cli) -> anyhow::Result<()> {
     let engine = Engine::default();
     let mut linker = Linker::new(&engine);
 
-    let mut     store = Store::new(&engine, Greet::new("Native code".to_string()));
+    let mut store = Store::new(&engine, Greet::new("Native code".to_string()));
 
     let component = Component::from_file(&engine, &cli.wasm_file)?;
 
     HelloWorld::add_to_linker(&mut linker, |greet: &mut Greet| greet)?;
+
+    wasmtime_wasi::add_to_linker_sync(&mut linker)?;
 
     let hello_world = HelloWorld::instantiate(&mut store, &component, &linker)?;
 
